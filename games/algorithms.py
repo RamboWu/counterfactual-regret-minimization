eb9ec593f9e4273fe005011b9ba85408872b31d4
Rambo <RamboWu90@gmail.com>
2021-12-24 17:09:06 +0800

    删除一些没用的东西，什么 纳什均衡值，这些都不重要，然后发现先手玩家拿到Q的时候，bet和 check的 比例有问题，Q不应该去打

--- games/algorithms.py
+++ games/algorithms.py
@@ -1,4 +1,4 @@
-from common.constants import A
+from common.constants import A, QK, QJ
 from common.utils import init_sigma, init_empty_node_maps
 
 class CounterfactualRegretMinimizationBase:
@@ -7,57 +7,33 @@ class CounterfactualRegretMinimizationBase:
         self.root = root
         self.sigma = init_sigma(root)
         self.cumulative_regrets = init_empty_node_maps(root)
-        self.cumulative_sigma = init_empty_node_maps(root)
-        self.nash_equilibrium = init_empty_node_maps(root)
         self.chance_sampling = chance_sampling
 
     def print_sigma(self):
         #print('when k comes:', self.sigma['.K.'])
         for info in self.sigma:
             for action in self.sigma[info]:
-                print(info, ' ', action, ' ', self.sigma[info][action])
+                print(info, ' |A:', action, ' |S:', self.sigma[info][action])
 
     def print_sigma_and_regrets(self):
         for info in self.sigma:
             for action in self.sigma[info]:
-                print(info, ' ', action,
-                ' s:', self.sigma[info][action] ,
-                ' r:', self.cumulative_regrets[info][action])
+                print(info, ' |A:', action,
+                ' |S:', self.sigma[info][action] ,
+                ' |R:', self.cumulative_regrets[info][action])
 
     def _update_sigma(self, i):
         rgrt_sum = sum(filter(lambda x : x > 0, self.cumulative_regrets[i].values()))
         for a in self.cumulative_regrets[i]:
             self.sigma[i][a] = max(self.cumulative_regrets[i][a], 0.) / rgrt_sum if rgrt_sum > 0 else 1. / len(self.cumulative_regrets[i].keys())
 
-    def compute_nash_equilibrium(self):
-        self.__compute_ne_rec(self.root)
-
-    def __compute_ne_rec(self, node):
-        if node.is_terminal():
-            return
-        i = node.inf_set()
-        if node.is_chance():
-            self.nash_equilibrium[i] = {a:node.chance_prob() for a in node.actions}
-        else:
-            sigma_sum = sum(self.cumulative_sigma[i].values())
-            self.nash_equilibrium[i] = {a: self.cumulative_sigma[i][a] / sigma_sum for a in node.actions}
-        # go to subtrees
-        for k in node.children:
-            self.__compute_ne_rec(node.children[k])
-
     def _cumulate_cfr_regret(self, information_set, action, regret):
         self.cumulative_regrets[information_set][action] += regret
-
-    def _cumulate_sigma(self, information_set, action, prob):
-        self.cumulative_sigma[information_set][action] += prob
 
     def run(self, iterations):
         raise NotImplementedError("Please implement run method")
 
-    def value_of_the_game(self):
-        return self.__value_of_the_game_state_recursive(self.root)
-
-    def _cfr_utility_recursive(self, state, reach_a, reach_b):
+    def _cfr_utility_recursive(self, state, reach_a, reach_b, need_print = False):
         children_states_utilities = {}
         if state.is_terminal():
             # evaluate terminal node according to the game result
@@ -75,7 +51,7 @@ class CounterfactualRegretMinimizationBase:
             child_reach_a = reach_a * (self.sigma[state.inf_set()][action] if state.to_move == A else 1)
             child_reach_b = reach_b * (self.sigma[state.inf_set()][action] if state.to_move == -A else 1)
             # value as if child state implied by chosen action was a game tree root
-            child_state_utility = self._cfr_utility_recursive(state.play(action), child_reach_a, child_reach_b)
+            child_state_utility = self._cfr_utility_recursive(state.play(action), child_reach_a, child_reach_b, need_print)
             # value computation for current node
             value +=  self.sigma[state.inf_set()][action] * child_state_utility
             # values for chosen actions (child nodes) are kept here
@@ -87,24 +63,16 @@ class CounterfactualRegretMinimizationBase:
             # we multiply regret by -1 for player B, this is because value is computed from player A perspective
             # again we need that perspective switch
             action_cfr_regret = state.to_move * cfr_reach * (children_states_utilities[action] - value)
-            if action_cfr_regret < 0:
+            if action_cfr_regret <= 0:
                 action_cfr_regret = 0
+            if need_print:
+                print(state.inf_set(), '|A:', action, '|G:', action_cfr_regret)
             self._cumulate_cfr_regret(state.inf_set(), action, action_cfr_regret)
-            self._cumulate_sigma(state.inf_set(), action, reach * self.sigma[state.inf_set()][action])
         if self.chance_sampling:
             # update sigma according to cumulative regrets - we can do it here because we are using chance sampling
             # and so we only visit single game_state from an information set (chance is sampled once)
             self._update_sigma(state.inf_set())
         return value
-
-    def __value_of_the_game_state_recursive(self, node):
-        value = 0.
-        if node.is_terminal():
-            return node.evaluation()
-        for action in node.actions:
-            value +=  self.nash_equilibrium[node.inf_set()][action] * self.__value_of_the_game_state_recursive(node.play(action))
-        return value
-
 
 class VanillaCFR(CounterfactualRegretMinimizationBase):
 
@@ -137,3 +105,8 @@ class ChanceSamplingCFR(CounterfactualRegretMinimizationBase):
     def run(self, iterations = 1):
         for _ in range(0, iterations):
             self._cfr_utility_recursive(self.root, 1, 1)
+
+    def test_run(self):
+        for _ in range(0, 1000):
+            self._cfr_utility_recursive(self.root, 1, 1)
+        self._cfr_utility_recursive(self.root.children[QK], 1, 1, True)
--- run.py
+++ run.py
@@ -5,9 +5,10 @@ from games.algorithms import ChanceSamplingCFR, VanillaCFR
 
 root = KuhnRootChanceGameState(CARDS_DEALINGS)
 chance_sampling_cfr = ChanceSamplingCFR(root)
-chance_sampling_cfr.run(iterations = 10000)
-chance_sampling_cfr.compute_nash_equilibrium()
-chance_sampling_cfr.print_sigma()
+#chance_sampling_cfr.run(iterations = 1000)
+chance_sampling_cfr.test_run()
+#chance_sampling_cfr.compute_nash_equilibrium()
+#chance_sampling_cfr.print_sigma()
 # read Nash-Equilibrum via chance_sampling_cfr.nash_equilibrium member
 # try chance_sampling_cfr.value_of_the_game() function to get value of the game (-1/18)
 
